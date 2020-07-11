e6d888fec1795d079683f5b0c183aea9a5f4d25a
DiegoRamos1 <diegoeduardoramos@gmail.com>
2020-07-10 23:31:34 -0400

    hola

--- miweb/models.py
+++ miweb/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Covid(models.Model):
+    dia = models.DateTimeField()
+    total_contagios = models.CharField(max_length=100)
+    total_recuperados = models.CharField(max_length=100)
+    diarios = models.CharField(max_length=100)
+
+    def publish(self):
+        self.save()
